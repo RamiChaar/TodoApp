7fe3337ef6ea2e69cde9d449aaf9e8d309b2e88c
RamiChaar <Chaarrami22@gmail.com>
2022-09-22 15:13:30 -0700

    Update ColorList.js

--- src/ColorList.js
+++ src/ColorList.js
@@ -1,8 +1,8 @@
 
-export default function ColorList({colors, setColor, handleSettings}) {
+export default function ColorList(props) {
 
     function chooseColor(event) {
-        setColor(color => {
+        props.setColor(color => {
             let hue = event.target.getAttribute('hue')
             let sat = event.target.getAttribute('sat')
             let light = event.target.getAttribute('light')
@@ -11,11 +11,11 @@ export default function ColorList({colors, setColor, handleSettings}) {
         document.documentElement.style.setProperty('--hue', event.target.getAttribute('hue'))
         document.documentElement.style.setProperty('--sat', event.target.getAttribute('sat')+"%")
         document.documentElement.style.setProperty('--lightness', event.target.getAttribute('light')+"%")
-        handleSettings()
+        props.handleSettings()
     }
 
     return (
-      colors.map(color => {
+      props.colors.map(color => {
         return <div className='color' id={color.id} key={color.id} hue={color.hue} sat={color.sat} light={color.light} onClick={chooseColor}></div>
       })
     )
